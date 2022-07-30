e9f4b6f0d88382c65faaab987b15835db91e86ca
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-07-30 01:38:51 +0000

    [Update] ページの色をボタンで変えられるようにした(改善の余地あり)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :color
+
+  def color
+    @color = "black"
+    @params = params[:bg_color]
+    if @params.present?
+      @color = @params
+    end
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,9 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
+import '@fortawesome/fontawesome-free/js/all'
+import "test.js"
+import '../stylesheets/test.css'
 
 Rails.start()
 Turbolinks.start()
